7aa2ff682f806afa4dbbfe55e03a47f3f3a382d9
Murillo Faria <murilofs14@gmail.com>
2026-07-03 08:12:51 -0300

    Correção do uso de variáveis de ambiente (.env)

--- app.py
+++ app.py
@@ -1,17 +1,132 @@
-from flask import Flask
-from database import conectar
+import os
+from flask import Flask, render_template, request, redirect, session
+from dotenv import load_dotenv
+
+from database import (
+    validar_login,
+    inserir_chamado,
+    listar_chamados,
+    atualizar_status,
+    cancelar_chamado
+)
+
+load_dotenv()
 
 app = Flask(__name__)
-
-app.secret_key = "123"
+app.secret_key = os.getenv("SECRET_KEY")
 
 
 @app.route("/")
-def inicio():
+def home():
 
-    conexao = conectar()
+    if "usuario" not in session:
+        return redirect("/login")
 
-    return "Sistema HelpDesk funcionando!"
+    return render_template(
+        "index.html",
+        usuario=session["usuario"]
+    )
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        usuario = request.form["usuario"]
+        senha = request.form["senha"]
+
+        resultado = validar_login(usuario, senha)
+
+        if resultado:
+
+            session["usuario"] = usuario
+            return redirect("/")
+
+        return render_template(
+            "login.html",
+            erro="Usuário ou senha inválidos."
+        )
+
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+
+    session.clear()
+
+    return redirect("/login")
+
+
+@app.route("/novo", methods=["GET", "POST"])
+def novo():
+
+    if "usuario" not in session:
+        return redirect("/login")
+
+    if request.method == "POST":
+
+        cliente = request.form["cliente"]
+        descricao = request.form["descricao"]
+        prioridade = request.form["prioridade"]
+
+        inserir_chamado(
+            cliente,
+            descricao,
+            prioridade
+        )
+
+        return redirect("/fila")
+
+    return render_template("novo_chamado.html")
+
+
+@app.route("/fila")
+def fila():
+
+    if "usuario" not in session:
+        return redirect("/login")
+
+    chamados = listar_chamados()
+
+    return render_template(
+        "chamados.html",
+        chamados=chamados
+    )
+
+
+@app.route("/atender/<int:id>")
+def atender(id):
+
+    if "usuario" not in session:
+        return redirect("/login")
+
+    atualizar_status(id, "Em Atendimento")
+
+    return redirect("/fila")
+
+
+@app.route("/concluir/<int:id>")
+def concluir(id):
+
+    if "usuario" not in session:
+        return redirect("/login")
+
+    atualizar_status(id, "Resolvido")
+
+    return redirect("/fila")
+
+
+@app.route("/cancelar/<int:id>")
+def cancelar(id):
+
+    if "usuario" not in session:
+        return redirect("/login")
+
+    cancelar_chamado(id)
+
+    return redirect("/fila")
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -4,25 +4,141 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-print("HOST =", os.getenv("DB_HOST"))
-print("PORT =", os.getenv("DB_PORT"))
-print("DATABASE =", os.getenv("DB_NAME"))
-print("USER =", os.getenv("DB_USER"))
-print("PASSWORD =", os.getenv("DB_PASSWORD"))
 
 def conectar():
+
     try:
+
         conexao = psycopg2.connect(
-            host="localhost",
-            port=5432,
-            dbname="helpdesk",
-            user="postgres",
-            password="postgres"
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD")
         )
 
         print("Banco conectado com sucesso!")
+
         return conexao
 
     except Exception as erro:
+
         print("Erro ao conectar:")
         print(repr(erro))
+
+
+def validar_login(nome, senha):
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute(
+        """
+        SELECT *
+        FROM usuarios
+        WHERE nome=%s
+        AND senha=%s
+        """,
+        (nome, senha)
+    )
+
+    usuario = cursor.fetchone()
+
+    cursor.close()
+    conexao.close()
+
+    return usuario
+
+
+def inserir_chamado(cliente, descricao, prioridade):
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute(
+        """
+        INSERT INTO chamados
+        (cliente, descricao, prioridade, status, statusfinal)
+
+        VALUES (%s, %s, %s, %s, %s)
+        """,
+        (
+            cliente,
+            descricao,
+            prioridade,
+            "Aberto",
+            False
+        )
+    )
+
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+
+
+def listar_chamados():
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute(
+        """
+        SELECT
+            id,
+            cliente,
+            descricao,
+            prioridade,
+            status
+        FROM chamados
+        WHERE statusfinal = FALSE
+        ORDER BY id
+        """
+    )
+
+    chamados = cursor.fetchall()
+
+    cursor.close()
+    conexao.close()
+
+    return chamados
+
+
+def atualizar_status(id, status):
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute(
+        """
+        UPDATE chamados
+        SET status=%s
+        WHERE id=%s
+        """,
+        (status, id)
+    )
+
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+
+
+def cancelar_chamado(id):
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute(
+        """
+        UPDATE chamados
+        SET statusfinal=TRUE
+        WHERE id=%s
+        """,
+        (id,)
+    )
+
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
